dc125156c3358ea5e0eb7efb813c3149904ec72c
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-20 19:15:42 -0300

    React components

---
+++ aula1/src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Button extends Component {
+    render() {
+        return(
+            <button>Salvar</button>
+        );
+    };
+};
+
+export default Button;
--- aula1/src/index.js
+++ aula1/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Button from './components/Button';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>
-                Hello React!!
-            </h1>
+            <>
+                <h1>
+                    Hello React!!
+                </h1>
+                <Button/>
+            </>
         );
     }
 }
