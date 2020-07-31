1290d0f55862335767e04bada4cb38e2c9e63dfe
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-30 21:33:57 -0300

    react router dom and api

--- aula2/src/App.js
+++ aula2/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from './component/Button';
+import { Link } from 'react-router-dom';
+import Personagem from './Personagem';
 
-function App() {
-   return (
-      <div></div>
-   );
-}
+class App extends Component {
+   state = {
+      personagens: []
+   };
+
+   async componentWillMount() { // Executa antes do render
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon'); // requisição api
+        const dataJson = await response.json(); // transforma o contéudo da requisição em um objeto json
+
+        this.setState({ personagens: dataJson.results }); // Salva em personagens todos personagens retornados
+      } catch (error) {
+        alert(error);
+      }
+    }
+   render() {
+      return (
+         <div>
+            {
+               this.state.personagens.map(personagem => { // Funciona como um loop. Mostra o nome de cada personagem na tela
+                  return(
+                     <div key={personagem.url}>
+                        {personagem.name}
+                     </div>
+                  )
+               })
+            }
+         </div>
+      );
+   };
+};
 
 export default App;
---
+++ aula2/src/Personagem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Personagem = (props) => (
+   <div>
+      <h1>Personagem</h1>
+      <Link to="/">Home</Link>
+   </div>
+
+);
+
+export default Personagem;
